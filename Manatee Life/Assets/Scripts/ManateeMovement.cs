be56807c58b7267f288488687ada2290cae270fd
Leo Velosa <64762864+LeoVelosa@users.noreply.github.com>
2021-03-24 00:12:54 -0400

    underwater works

---
+++ Manatee Life/Assets/Scripts/ManateeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManateeMovement : MonoBehaviour
+{
+    float speed = 6f;
+    float lookSpeed = 6f;
+    public Transform cam;
+    void Start()
+    {
+        //cam = GetComponent<Transform>();
+    }
+    void Update()
+    {
+
+        //this.transform.position = ((right + forward) * speed);
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //movement = cam.TransformDirection(movement);
+        //movement = Vector3.ProjectOnPlane(movement, Vector3.up);
+
+        Vector3 camDirection = cam.forward;
+        camDirection = Vector3.ProjectOnPlane(camDirection, Vector3.up);
+
+        this.transform.forward += camDirection;
+    }
+}
