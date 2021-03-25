4c3134aa5b56c7d458b09b523a575c89ea97126a
Leo Velosa <64762864+LeoVelosa@users.noreply.github.com>
2021-03-25 02:00:33 -0400

    UI/game components

---
+++ Manatee Life/Assets/Scripts/AirBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AirBar : MonoBehaviour
+{
+    public Slider AirBarSlider;
+    public Air airLeft;
+
+    void Start()
+    {
+        airLeft = GameObject.Find("Main Camera").GetComponent<Air>();
+        AirBarSlider = this.GetComponent<Slider>();
+        AirBarSlider.maxValue = airLeft.maxAir;
+        AirBarSlider.value = airLeft.currentAir;
+    }
+
+    public void setAir(float airAmount)
+    {
+        AirBarSlider.value = airAmount;
+    }
+}
